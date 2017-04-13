2762802d572db9e823e6f83333ad2d8f5689e8be
Acky <acky@xian62.com>
2017-04-13 13:17:08 +0800

    hanlde situation of template contain \n

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ class Temp {
 		let match = null
 		let temp = `let str = ''`
 		const addCode = (str, isOutput) => isOutput ? temp += `\nstr+=${str}` : temp += `\n${str}`
-		const addStr = str => temp += `\nstr+='${str}'`
+		const addStr = str => { 
+			const trimStr = str.trim()
+			if(trimStr !== '') temp += `\nstr+='${str.trim().replace(/\n/g, '')}'`
+		}
 		while(match = r.exec(str)) {
-			const betweenStr = str.substring(indexBefore, match.index).trim()
-			betweenStr !== '' && addStr(betweenStr, true)
+			addStr(str.slice(indexBefore, match.index))
 			;/^<%=/.test(match[0]) ? addCode(match[1],true) : addCode(match[1])
 			indexBefore = match.index + match[0].length
 		}
+		addStr(str.slice(indexBefore, str.length-indexBefore))
 		temp += `\nreturn str`
 		// console.log(temp)
 		return (data) => {
